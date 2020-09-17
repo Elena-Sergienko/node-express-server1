cc80f4298e78bd04a9150fa120ae90ab25964d7a
Elena-Sergienko <elena.serg.us@gmail.com>
2020-09-17 00:37:43 -0700

    Update (s.6 #5 User Get All)

--- src/modules/user/Routes.js
+++ src/modules/user/Routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import userRegister from './userRegister';
+import userGetAll from './userGetAll';
 
 const router = Router();
 
 router.post('/', userRegister); // POST: localhost:5002/user
+router.get('/', userGetAll); // POST: localhost:5002/user
 
 
 export default router;
