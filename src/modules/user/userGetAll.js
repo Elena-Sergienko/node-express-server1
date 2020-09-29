2c17c73c27d20add0e44526a447151dd8c3cf2a1
Elena-Sergienko <elena.serg.us@gmail.com>
2020-09-28 21:59:29 -0700

    Update (s.6 #6 User Get All. Filter, limit, skip)

--- src/modules/user/userGetAll.js
+++ src/modules/user/userGetAll.js
@@ -2,7 +2,9 @@ import User from './Model';
 
 export default function useGetAll(req, res) {
   User
-    .find()
+    .find({email: '1234@123'})
+    .limit(2)
+    .skip(1)
     .exec()
     .then(result => {
       res.status(200).json(result);
