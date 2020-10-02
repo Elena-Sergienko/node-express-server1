90c4641aeebe28ad043712ab058b805317be85f9
Elena-Sergienko <elena.serg.us@gmail.com>
2020-10-01 22:20:37 -0700

    Update (s.6 #10) User Delete by id

--- src/modules/user/Routes.js
+++ src/modules/user/Routes.js
@@ -3,6 +3,7 @@ import userRegister from './userRegister';
 import userGetAll from './userGetAll';
 import userGetById from './userGetById';
 import userUpdateById from './userUpdateById';
+import userDeleteById from './userDeleteById';
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.post('/', userRegister); // POST: localhost:5002/user
 router.get('/', userGetAll); // GET: localhost:5002/user
 router.get('/:userId', userGetById); // GET: localhost:5002/user/989998...
 router.patch('/:userId', userUpdateById); // PATCH: localhost:5002/user/9899898...
+router.delete('/:userId', userDeleteById); // Delete: localhost:5002/user/9899898...
 
 
 export default router;
